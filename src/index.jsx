e70272499fbba0b5c93c0a483d96c9e78d642510
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2023-08-07 08:57:30 -0300

    Updating ReactDOM and Redux store

--- src/index.jsx
+++ src/index.jsx
@@ -2,32 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals'
 
-import { createStore } from 'redux'
 
 import { Provider } from 'react-redux'
 
-import rootReducer from './redux/reducers'
+import {reducer} from './redux/reducers'
 
 import './assets/boxicons-2.0.7/css/boxicons.min.css'
 import './assets/css/grid.css'
 import './assets/css/theme.css'
 import './assets/css/index.css'
+import { createRoot } from 'react-dom/client';
 
 import Layout from './components/layout/Layout'
+import { configureStore } from '@reduxjs/toolkit';
 
-const store = createStore(
-  rootReducer
+const store = configureStore(
+  {reducer}
 )
 
 document.title = 'CreativeLabs Dashboard'
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <Layout />
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
